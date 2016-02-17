5ce3ce4f44ab6f13ee586916d9535fe5eb3488b5
alfg <alf.g.jr@gmail.com>
2016-02-16 22:38:58 -0800

    Linting fixes.

--- dist/ping.js
+++ dist/ping.js
@@ -19,17 +19,22 @@ var Ping = function() {
  * Pings source and triggers a callback when completed.
  * @param source Source of the website or server.
  * @param callback Callback function to trigger when completed.
+ * @param timeout Optional number of milliseconds to wait before aborting.
  */
-Ping.prototype.ping = function(source, callback) {
+Ping.prototype.ping = function(source, callback, timeout) {
     this.img = new Image();
+    timeout = timeout || 0;
+    var timer;
 
     var start = new Date();
     this.img.onload = this.img.onerror = pingCheck;
+    if (timeout) { timer = setTimeout(pingCheck, timeout); }
 
     /**
      * Times ping and triggers callback.
      */
     function pingCheck() {
+        if (timer) { clearTimeout(timer); }
         var pong = new Date() - start;
 
         if (typeof callback === "function") {
@@ -39,4 +44,3 @@ Ping.prototype.ping = function(source, callback) {
 
     this.img.src = "//" + source + "/?" + (+new Date()); // Trigger image load with cache buster
 };
-
